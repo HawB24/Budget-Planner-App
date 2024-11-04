a105d764d264ec12d2ae81a9a3d424dd01619ad7
HawB24 <loneacc195@gmail.com>
2024-11-04 22:43:59 +0700

    fix-color

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function HomeLayout() {
+  const [fontsLoaded] = useFonts({
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+  });
+
+  // Wait until fonts are loaded
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <Stack
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="add-new-category"
+        options={{
+          presentation: "modal",
+          headerShown: true,
+          headerTitle: "Add New Category",
+        }}
+      />
     </Stack>
   );
 }
